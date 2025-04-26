e4adf062fd4f0560901ee5dd4ce36a8e135ab4de
Huy Nhat Ngo <huyngo248@gmail.com>
2025-04-26 23:31:23 +0900

    feat: refactor NavigationMenu to use usePathname and useParams for dynamic logo display

--- src/components/shared/navigation-menu/index.tsx
+++ src/components/shared/navigation-menu/index.tsx
@@ -1,11 +1,14 @@
+'use client'
+
 import { Dictionary } from '@/types'
 import { cn } from '@/lib/utils'
 import DesktopNavMenu from './desktop-nav-menu'
 import MobileNavMenu from './mobile-nav-menu'
+import { usePathname, useParams } from 'next/navigation'
 
 interface NavigationMenuProps {
   className?: string
-  displayLogo?: boolean
+
   dictionary: Dictionary
 }
 
@@ -28,18 +31,21 @@ export const handleClick = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>, 
   }
 }
 
-export default function NavigationMenu({
-  className,
-  dictionary,
-  displayLogo = false,
-}: NavigationMenuProps) {
+export default function NavigationMenu({ className, dictionary }: NavigationMenuProps) {
+  const pathname = usePathname()
+  const params = useParams()
+
   return (
     <nav
       id="navigation-menu"
       className={cn('sticky top-0 z-50 text-white uppercase bg-black/20', className)}
     >
       <div className="w-container">
-        <DesktopNavMenu className="py-6" dictionary={dictionary} displayLogo={displayLogo} />
+        <DesktopNavMenu
+          className="py-6"
+          dictionary={dictionary}
+          displayLogo={pathname !== `/${params.lang}`}
+        />
         <MobileNavMenu className="py-4" dictionary={dictionary} />
       </div>
     </nav>
