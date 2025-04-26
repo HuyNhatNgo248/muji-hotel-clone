d5bfe399cf470fe4bf01e77b904437bdb307a1e4
Huy Nhat Ngo <huyngo248@gmail.com>
2025-04-26 14:09:31 +0900

    feat: add remotePatterns to next.config for image handling from Vercel Blob storage

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,17 @@ import { withPayload } from '@payloadcms/next/withPayload'
 const nextConfig = {
   // Your Next.js config here
   output: 'standalone',
+  remotePatterns: [
+    {
+      protocol: 'https',
+      hostname: 'q4aw4kksczq5nsez.public.blob.vercel-storage.com', // Your deployed domain
+      pathname: '/api/media/file/**',
+    },
+    {
+      protocol: 'http',
+      hostname: '*', // Allow images from all HTTP domains
+    },
+  ],
 }
 
 export default withPayload(nextConfig, { devBundleServerPackages: false })
