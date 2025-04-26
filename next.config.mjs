ddc666347972cd13d62da17bf65a7de3cc586fa5
Huy Nhat Ngo <huyngo248@gmail.com>
2025-04-26 14:44:42 +0900

    feat: move remotePatterns configuration under images in next.config

--- next.config.mjs
+++ next.config.mjs
@@ -4,17 +4,19 @@ import { withPayload } from '@payloadcms/next/withPayload'
 const nextConfig = {
   // Your Next.js config here
   output: 'standalone',
-  remotePatterns: [
-    {
-      protocol: 'https',
-      hostname: 'q4aw4kksczq5nsez.public.blob.vercel-storage.com', // Your deployed domain
-      pathname: '/api/media/file/**',
-    },
-    {
-      protocol: 'http',
-      hostname: '*', // Allow images from all HTTP domains
-    },
-  ],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'q4aw4kksczq5nsez.public.blob.vercel-storage.com', // Your deployed domain
+        pathname: '/api/media/file/**',
+      },
+      {
+        protocol: 'http',
+        hostname: '*', // Allow images from all HTTP domains
+      },
+    ],
+  },
 }
 
 export default withPayload(nextConfig, { devBundleServerPackages: false })
